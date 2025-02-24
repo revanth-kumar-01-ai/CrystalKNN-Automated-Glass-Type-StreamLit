eb9748cdc004fdd0665a24ae272c38a0a43388c2
revanth-kumar-01-ai <revanthj486@gmail.com>
2025-02-24 17:02:10 +0530

    updates features and fixed bugs

--- ViewPage/Data_Exploration.py
+++ ViewPage/Data_Exploration.py
@@ -4,36 +4,26 @@ import plotly.express as px
 
 st.title("Data Exploration - CrystalKNN")
 
-# File uploader
-uploaded_file = 'data/glass.csv'
+# Show first 5 rows
+st.subheader("Dataset Head (First 5 Rows)")
+st.image('./assets/head5.png')
 
-if uploaded_file:
-    df = pd.read_csv(uploaded_file)
+# Show dataset description
+st.subheader("Dataset Description")
+st.image('./assets/description.png')
 
-    # Show first 5 rows
-    st.subheader("Dataset Head (First 5 Rows)")
-    st.write(df.head())
-    
-    # Show dataset description
-    st.subheader("Dataset Description")
-    st.write(df.describe())
-    
-    # Select a categorical column for bar chart
-    st.subheader("Bar Chart Visualization - Target Values")
-    target = 'Type'
-    
-    if target in df.columns:
-        value_counts = df[target].value_counts().reset_index()
-        value_counts.columns = ["Category", "Count"]
-        fig = px.bar(
-            value_counts,
-            x="Category",
-            y="Count",
-            color="Category",
-            text="Count",
-            title=f"Bar Chart of {target}"
-        )
-        fig.update_traces(textposition='outside')
-        st.plotly_chart(fig, use_container_width=True)
-    else:
-        st.write("Target column not found in dataset.")
+# Select a categorical column for bar chart
+st.subheader("Bar Chart Visualization - Target Values")
+
+value_counts = pd.DataFrame({'Category': ["2", "1", "7", "3", "5", "8"], 'Count': [76, 70, 29, 17, 13, 9]})
+
+fig = px.bar(
+        value_counts,
+        x="Category",
+        y="Count",
+        color="Category",
+        text="Count",
+        title=f"Bar Chart of Type"
+    )
+fig.update_traces(textposition='outside')
+st.plotly_chart(fig, use_container_width=True)
